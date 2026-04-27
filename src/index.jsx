d56d16ee226271013a6d00bca0bf176e3a2aa40a
unknown <nurudeenkeen@gmail.com>
2026-04-27 19:12:46 +0100

    feat: add ssg functionality

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import * as serviceWorker from '@/serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  // If there's already content, hydrate it
+  ReactDOM.hydrateRoot(rootElement, <App />);
+} else {
+  // Otherwise, render normally (Standard CSR)
+  ReactDOM.createRoot(rootElement).render(<App />);
+}
 
 serviceWorker.register();
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePrerender from "vite-plugin-prerender";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePrerender({
+      // The path to the folder where Vite outputs your build
+      staticDir: path.join(__dirname, "dist"),
+      // The list of routes you want to prerender
+      routes: ["/", "/work", "/resume", "/articles", "/contact"],
+      // Optional: Minify the resulting HTML
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        decodeEntities: true,
+        keepClosingSlash: true,
+        sortAttributes: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
   server: {
     port: 3000, // Set your desired port (default is 5173)
     open: true, // Automatically open the app in the browser
   },
-})
+});
