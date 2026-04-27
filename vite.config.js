940bb133e9cc2137641a4027a99b744e4928d70d
unknown <nurudeenkeen@gmail.com>
2026-04-27 20:03:04 +0100

    fix: resolve build failure:take-4

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig(async ({ mode }) => {
   // Determine if we are running in a CI/Vercel environment
   const isVercel = process.env.VERCEL === "1";
 
+  const puppeteerOptions = isVercel
+    ? {
+        args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
+      }
+    : {};
+
   return {
     plugins: [
       react(),
@@ -23,13 +31,7 @@ export default defineConfig(async ({ mode }) => {
         },
         // Puppeteer settings for Serverless compatibility
         renderOptions: {
-          launchOptions: isVercel
-            ? {
-                args: chromium.args,
-                executablePath: await chromium.executablePath(),
-                headless: chromium.headless,
-              }
-            : {}, // Uses default local chrome in development
+          launchOptions: puppeteerOptions,
         },
       }),
     ],
